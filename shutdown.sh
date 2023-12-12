55d2ac411ba89f700998bb4b99b1d4eeca81af34
dll <kaixin890312@163.com>
2023-12-12 21:26:07 +0800

    update shell

--- shutdown.sh
+++ shutdown.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose -f docker-compose.yml -f docker-compose.canvas.yml -p canvas-lms down --rmi all
+docker compose -f docker-compose.yml -f docker-compose.canvas.yml -p canvas-lms down # --rmi all
