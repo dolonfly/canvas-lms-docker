32614d53f5b5dd17ff583f689170b17f574e2ad5
dll <kaixin890312@163.com>
2024-02-26 14:21:02 +0800

    .

---
+++ canvas/script/prod-db-initial.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /opt/canvas
+mv db/migrate/20210823222355_change_immersive_reader_allowed_on_to_on.rb .;
+mv db/migrate/20210812210129_add_singleton_column.rb db/migrate/20111111214311_add_singleton_column.rb;
+RAILS_ENV=production bundle exec rake db:initial_setup;
+
+mv 20210823222355_change_immersive_reader_allowed_on_to_on.rb db/migrate/.;
+RAILS_ENV=production bundle exec rake db:migrate;
--- setup_init.sh
+++ setup_init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose -f docker-compose.yml -f docker-compose.canvas.yml -p canvas-lms run --rm web bundle exec rake db:initial_setup
+docker compose -f docker-compose.yml -f docker-compose.canvas.yml -p canvas-lms run --rm web sh /opt/canvas/prod-db-initial.sh
