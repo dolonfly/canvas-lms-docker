a10e3979c19584fde9094ec711b0d89b0ba175e2
dll <kaixin890312@163.com>
2024-02-28 02:29:25 +0800

    jog

--- canvas/config/environments/production-local.rb
+++ canvas/config/environments/production-local.rb
@@ -1,3 +1,4 @@
 config.cache_classes = true
 config.action_controller.perform_caching = true
 config.action_view.cache_template_loading = true
+config.log_level = :debug
--- canvas/script/start-prod.sh
+++ canvas/script/start-prod.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+/etc/init.d/canvas_init start
 rm -f /var/run/apache2/apache2.pid
 /usr/sbin/apache2ctl -D FOREGROUND -e info
