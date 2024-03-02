d84ae72d237beaafb927ed70b2c69c0e7a9041ce
dll <kaixin890312@163.com>
2024-03-03 00:47:17 +0800

    fix

--- canvas/script/prod-db-initial.sh
+++ canvas/script/prod-db-initial.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt/canvas
+cd /home/app/canvas
 mv db/migrate/20210823222355_change_immersive_reader_allowed_on_to_on.rb .;
 mv db/migrate/20210812210129_add_singleton_column.rb db/migrate/20111111214311_add_singleton_column.rb;
 RAILS_ENV=production bundle exec rake db:initial_setup;
